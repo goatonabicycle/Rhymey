68e6e54ae75e4ace188a770ccebd8efde3632619
Rowan <malice.peanutbutter@gmail.com>
2018-10-25 14:46:28 +0200

    Don't check on phrases, clear on backspace, near rhyme mode.

--- app/js/content.js
+++ app/js/content.js
@@ -4,41 +4,54 @@ let wordMustBeAtLeastThisLong = 2
 
 document.addEventListener('dblclick', () => {
   let sel = window.getSelection().toString()
-  if (sel.length && sel.trim().length > wordMustBeAtLeastThisLong) {
+  if (
+    sel.length &&
+    sel.trim().length > wordMustBeAtLeastThisLong &&
+    sel.indexOf(' ') === -1
+  ) {
     selectedWord = sel.trim()
     getRhymingWords(selectedWord)
-  } else { selectedWord = '' }
+  } else {
+    selectedWord = ''
+  }
 })
 
-document.addEventListener('mouseup', (event) => {
+document.addEventListener('mouseup', event => {
   if (event.target.closest('.rhyme-popup-contain')) return
   popup.removeExistingPopup()
 })
 
-const getRhymingWords = (selectedWord) => {
-  fetch('https://api.datamuse.com/words?rel_rhy=' + selectedWord) // merge the results of 'nry=' here
-    .then(response => response.json())
-    .then(json => popup.addToPage(json))
+document.addEventListener('keydown', event => {
+  console.log('keydown')
+  popup.removeExistingPopup()
+})
+
+const getRhymingWords = selectedWord => {
+  var requestForRealRhymes = fetch('https://api.datamuse.com/words?rel_rhy=' + selectedWord).then(makeItJson)
+  var requestForNearRhymes = fetch('https://api.datamuse.com/words?rel_nry=' + selectedWord).then(makeItJson)
+
+  Promise.all([requestForRealRhymes, requestForNearRhymes]).then(function (values) {
+    popup.addToPage(values[0], values[1])
+  })
 }
 
 let popup = {
-  addToPage: (data) => {
-    let display = 'Nothing found :('
-    if (data.length > 0) {
-      display = ''
-      for (var i = 0; i < data.length; i++) {
-        display += '- ' + data[i].word + renderSeperator
-      }
-    }
+  addToPage: (realRhymes, nearRhymes) => {
+    let realRhymesDisplay = getDisplayData(realRhymes)
+    let nearRhymesDisplay = getDisplayData(nearRhymes)
 
     popup.removeExistingPopup()
     let container = document.createElement('div')
     container.id = 'RhymeContainer'
     container.className = 'rhyme-popup-contain'
     container.innerHTML =
-      "<div><div class='rhymey-title'>" +
-      'Rhyming with: <span class="word">' + selectedWord + '</span></div><br />' +
-      display.replace(/,\s*$/, '') +
+      '<div>' +
+      '<div class="word">' + selectedWord + '</div><br />' +
+      '<div class="rhymey-title">Rhymes: </div>' +
+      realRhymesDisplay.replace(/,\s*$/, '') + '<br /><br />' +
+      '<div class="rhymey-title">Near rhymes:</div>' +
+      nearRhymesDisplay.replace(/,\s*$/, '') +
+
       '</div>' // Todo: Get a better way of rendering this out
     document.body.appendChild(container)
   },
@@ -49,3 +62,16 @@ let popup = {
     }
   }
 }
+
+const getDisplayData = (data) => {
+  let display = 'Nothing found :('
+  if (data.length > 0) {
+    display = ''
+    for (var i = 0; i < data.length; i++) {
+      display += '- ' + data[i].word + renderSeperator
+    }
+  }
+  return display
+}
+
+const makeItJson = (response) => { return response.json() }
