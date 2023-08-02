417971f2b03f67c01aef4815347a4fd46a6396a8
Rowan <malice.peanutbutter@gmail.com>
2023-08-02 21:27:31 +0200

    A little better. Not quite there yet.

--- app/js/content.js
+++ app/js/content.js
@@ -51,25 +51,25 @@ function extractMatrixValues(transformMatrix) {
   };
 }
 
-// -----------------------------------------------------
+// // -----------------------------------------------------
 
-const getSelectedText = function () {
-  const overlaps = (a, b) =>
-    a.left < b.right &&
-    a.right > b.left &&
-    a.top < b.bottom &&
-    a.bottom > b.top;
-  const page = getCurrentlyVisiblePage(getPages());
-  const selectionRects = Array.from(
-    page.querySelectorAll(".kix-canvas-tile-selection > svg > rect")
-  ).map((el) => el.getBoundingClientRect());
-  return Array.from(page.querySelectorAll("svg > g[role=paragraph] > rect"))
-    .map((el) => ({ el: el, rect: el.getBoundingClientRect() }))
-    .filter((item) => selectionRects.some((rect) => overlaps(item.rect, rect)))
-    .map((item) => item.el.getAttribute("aria-label"))
-    .filter(makeDeduper())
-    .join(" ");
-};
+// const getSelectedText = function () {
+//   const overlaps = (a, b) =>
+//     a.left < b.right &&
+//     a.right > b.left &&
+//     a.top < b.bottom &&
+//     a.bottom > b.top;
+//   const page = getCurrentlyVisiblePage(getPages());
+//   const selectionRects = Array.from(
+//     page.querySelectorAll(".kix-canvas-tile-selection > svg > rect")
+//   ).map((el) => el.getBoundingClientRect());
+//   return Array.from(page.querySelectorAll("svg > g[role=paragraph] > rect"))
+//     .map((el) => ({ el: el, rect: el.getBoundingClientRect() }))
+//     .filter((item) => selectionRects.some((rect) => overlaps(item.rect, rect)))
+//     .map((item) => item.el.getAttribute("aria-label"))
+//     .filter(makeDeduper())
+//     .join(" ");
+// };
 
 function getPages() {
   const pages = Array.from(document.querySelectorAll(".kix-page-paginated"));
@@ -88,16 +88,16 @@ function getCurrentlyVisiblePage(pages) {
   throw new Error("Can't get the currently visible page");
 }
 
-function makeDeduper() {
-  let prev;
-  return function (text) {
-    if (text == prev) return false;
-    prev = text;
-    return true;
-  };
-}
+// function makeDeduper() {
+//   let prev;
+//   return function (text) {
+//     if (text == prev) return false;
+//     prev = text;
+//     return true;
+//   };
+// }
 
-// -----------------------------------------------------
+// // -----------------------------------------------------
 
 function getMousePosition(canvas, event) {
   let rect = canvas.getBoundingClientRect();
@@ -116,21 +116,24 @@ function isWithinWordBounds(mousePos, wordBounds) {
 // Fetches word from Google Docs
 function getWordFromGoogleDocs(event) {
   const canvas = document.querySelector("canvas");
-  const mousePosition = getMousePosition(canvas, event);
 
   const pages = getPages();
-  console.log({ pages });
+  console.log("all pages: ", pages);
 
   const page = getCurrentlyVisiblePage(pages);
-  console.log({ page });
+  console.log("current page: ", page);
 
-  const allNodesInThisDoc = page.querySelectorAll(
+  const mousePosition = getMousePosition(page, event);
+
+  const allNodesInThisDoc = document.querySelectorAll(
     ".kix-canvas-tile-content svg>g>rect"
   );
 
   console.log("allNodesInThisDoc.length:", allNodesInThisDoc.length);
 
   const ctx = canvas.getContext("2d");
+
+  let log = [];
 
   for (let node of allNodesInThisDoc) {
     const nodeText = node.getAttribute("aria-label");
@@ -159,12 +162,17 @@ function getWordFromGoogleDocs(event) {
 
       if (isWithinWordBounds(mousePosition, wordBounds)) {
         console.log("Clicked word:", word);
+        console.log({ mousePosition, word, wordWidth, wordBounds });
         return word;
       }
+
+      log.push({ mousePosition, word, wordWidth, wordBounds });
 
       wordStartX += wordWidth + ctx.measureText(" ").width;
     }
   }
+
+  console.log({ log });
 
   return "";
 }
@@ -261,144 +269,3 @@ const renderBlock = (title, data) => {
 const makeItJson = (response) => {
   return response.json();
 };
-
-/*
- * Copyright (C) 2023  Yomitan Authors
- * Copyright (C) 2022  Yomichan Authors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-/* global
- * DocumentUtil
- * TextSourceRange
- */
-
-/**
- * This class is a helper for handling Google Docs content in content scripts.
- */
-class GoogleDocsUtil {
-  /**
-   * Scans the document for text or elements with text information at the given coordinate.
-   * Coordinates are provided in [client space](https://developer.mozilla.org/en-US/docs/Web/CSS/CSSOM_View/Coordinate_systems).
-   * @param {number} x The x coordinate to search at.
-   * @param {number} y The y coordinate to search at.
-   * @param {GetRangeFromPointOptions} options Options to configure how element detection is performed.
-   * @returns {?TextSourceRange|TextSourceElement} A range for the hovered text or element, or `null` if no applicable content was found.
-   */
-  static getRangeFromPoint(x, y, { normalizeCssZoom }) {
-    const styleNode = this._getStyleNode();
-    styleNode.disabled = false;
-    const element = document.elementFromPoint(x, y);
-    styleNode.disabled = true;
-    if (
-      element !== null &&
-      element.matches(".kix-canvas-tile-content svg>g>rect")
-    ) {
-      const ariaLabel = element.getAttribute("aria-label");
-      if (typeof ariaLabel === "string" && ariaLabel.length > 0) {
-        return this._createRange(element, ariaLabel, x, y, normalizeCssZoom);
-      }
-    }
-    return null;
-  }
-
-  static _getStyleNode() {
-    // This <style> node is necessary to force the SVG <rect> elements to have a fill,
-    // which allows them to be included in document.elementsFromPoint's return value.
-    if (this._styleNode === null) {
-      const style = document.createElement("style");
-      style.textContent = [
-        ".kix-canvas-tile-content{pointer-events:none!important;}",
-        ".kix-canvas-tile-content svg>g>rect{pointer-events:all!important;}",
-      ].join("\n");
-      const parent = document.head || document.documentElement;
-      if (parent !== null) {
-        parent.appendChild(style);
-      }
-      this._styleNode = style;
-    }
-    return this._styleNode;
-  }
-
-  static _createRange(element, text, x, y, normalizeCssZoom) {
-    // Create imposter
-    const content = document.createTextNode(text);
-    const svgText = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "text"
-    );
-    const transform = element.getAttribute("transform") || "";
-    const font = element.getAttribute("data-font-css") || "";
-    svgText.setAttribute("x", element.getAttribute("x"));
-    svgText.setAttribute("y", element.getAttribute("y"));
-    svgText.appendChild(content);
-    const textStyle = svgText.style;
-    this._setImportantStyle(textStyle, "all", "initial");
-    this._setImportantStyle(textStyle, "transform", transform);
-    this._setImportantStyle(textStyle, "font", font);
-    this._setImportantStyle(textStyle, "text-anchor", "start");
-    element.parentNode.appendChild(svgText);
-
-    // Adjust offset
-    const elementRect = element.getBoundingClientRect();
-    const textRect = svgText.getBoundingClientRect();
-    const yOffset =
-      (elementRect.top -
-        textRect.top +
-        (elementRect.bottom - textRect.bottom)) *
-      0.5;
-    this._setImportantStyle(
-      textStyle,
-      "transform",
-      `translate(0px,${yOffset}px) ${transform}`
-    );
-
-    // Create range
-    const range = this._getRangeWithPoint(content, x, y, normalizeCssZoom);
-    this._setImportantStyle(textStyle, "pointer-events", "none");
-    this._setImportantStyle(textStyle, "opacity", "0");
-    return TextSourceRange.createFromImposter(range, svgText, element);
-  }
-
-  static _getRangeWithPoint(textNode, x, y, normalizeCssZoom) {
-    if (normalizeCssZoom) {
-      const scale = DocumentUtil.computeZoomScale(textNode);
-      x /= scale;
-      y /= scale;
-    }
-    const range = document.createRange();
-    let start = 0;
-    let end = textNode.nodeValue.length;
-    while (end - start > 1) {
-      const mid = Math.floor((start + end) / 2);
-      range.setStart(textNode, mid);
-      range.setEnd(textNode, end);
-      if (DocumentUtil.isPointInAnyRect(x, y, range.getClientRects())) {
-        start = mid;
-      } else {
-        end = mid;
-      }
-    }
-    range.setStart(textNode, start);
-    range.setEnd(textNode, start);
-    return range;
-  }
-
-  static _setImportantStyle(style, propertyName, value) {
-    style.setProperty(propertyName, value, "important");
-  }
-}
-// eslint-disable-next-line no-underscore-dangle
-GoogleDocsUtil._styleNode = null;
